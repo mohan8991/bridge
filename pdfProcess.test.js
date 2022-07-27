e7e79fac7255ca5a1466c7b341911d4759dfe873
mohan.gopikrishna <mohan.gopikrishna@servicenow.com>
2022-07-27 10:59:10 -0700

    restructure and use await async

--- pdfProcess.test.js
+++
@@ -1,9 +0,0 @@
-const pdfProcess = require('./pdfProcess');
-
-jest.useFakeTimers();
-  test('Validating row count from pdf', () => {
-    pdfProcess.processPdf("data/foo.pdf");
-    setTimeout(() => {
-        expect(pdfProcess.data.length).toBe('104')
-    },0)
-  });
