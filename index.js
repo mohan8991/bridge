252389f47535023f6435999c0c01d4b6468c8df3
mohan.gopikrishna <mohan.gopikrishna@servicenow.com>
2022-07-27 02:12:19 -0700

    adding output to xlsx reading from pdf

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const libPdf = require("./pdfProcess")
+
+const XLSX = require('xlsx')
+
 const fs = require('fs');
 
 const inputPdf = "data/foo.pdf";
-const output = "output/foo.json";
+const output = "./output/test.xlsx";
 
 const pdfData = [];
 const writeToFile = (fileName, data) => {
-    fs.writeFile(fileName, data, () => {console.log("Done.")});
+    fs.writeFile(fileName, JSON.stringify(data), () => {console.log("Done.")});
+}
+
+const writeToExcel = (jsonData) => {
+    const ws = XLSX.utils.json_to_sheet(jsonData)
+    const wb = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(wb,ws,"Assessment")
+    XLSX.writeFile(wb, output)
 }
 
 libPdf.processPdf(inputPdf);
-console.log(pdfData)
+setTimeout(() => {
+    pdfData.push(...libPdf.data);
+    writeToExcel(pdfData);
+}, 0);
+
+
+
--- pdfProcess.js
+++ pdfProcess.js
@@ -1,16 +1,33 @@
 const pdf_table_extractor = require("pdf-table-extractor");
 
-const data = []
+let data = []
 const success = (result) => {
+    let rawData = [];
     result.pageTables.forEach(element => {
-       data.push(...element.tables);
+        rawData.push(...element.tables);
     });
+    data.push(...formatData(rawData));
 }
 
 const error = (err) => {console.log("Error" + err);}
 
 const processPdf = (inputPdf) => {
     pdf_table_extractor(inputPdf, success, error);
+
+}
+
+const formatData = (data) => {
+
+    let formattedArr = []
+    var keyArr = data[0];
+    for(var i=2; i<data.length; i++) {
+        var obj = {}
+        for(var j=0;j<keyArr.length;j++){
+            obj[keyArr[j]] = data[i][j]
+        }
+    formattedArr.push(obj);
+    }
+    return formattedArr;
 }
 
 module.exports = { processPdf, data}
